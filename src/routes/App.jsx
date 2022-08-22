d2c4ecbc11ac35b49efcb5d8c6f91667e44d41be
Jose Iriza <joseairiza@gmail.com>
2022-08-22 15:41:48 -0300

    moved RecoveryPassword to pages folder, instead of being a container

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
 import Login from '../pages/Login';
-import RecoveryPassword from '../containers/RecoveryPassword';
+import RecoveryPassword from '../pages/RecoveryPassword';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 import '../styles/global.css';
