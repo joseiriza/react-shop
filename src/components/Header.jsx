33a26480231033798438121fdab277997d79fc1c
Jose Iriza <joseairiza@gmail.com>
2022-09-05 19:41:11 -0300

    fixed images issues and css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import '@styles/Header.scss';
 
-const logo = '@icons/icon_menu.svg';
-const menu = '@icons/icon_menu.svg';
-const shoppingCart = '@icons/icon_shopping_cart.svg';
+import menu from '@icons/icon_menu.svg';
+import logo from '@logos/logo_yard_sale.svg';
+import shoppingCart from '@icons/icon_shopping_cart.svg';
 
 const Header = () => {
     return(
         <nav>
             <img src={menu} alt="menu" className="menu" />
             <div className="navbar-left">
-                <img src={logo} alt="logo" className="logo" />
+                <img src={logo} alt="logo" className="nav-logo" />
                 <ul>
                     <li>
                         <a href="/">All</a>
