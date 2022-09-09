ed89e5a460041cfd220bfd89ad9fa437774bb625
Jose Iriza <joseairiza@gmail.com>
2022-09-09 19:31:37 -0300

    ProductItem component update to receive data from API

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -12,7 +12,7 @@ const ProductList = () => {
 		<section className="main-container">
 			<div className="ProductList">
 				{products.map(product => (
-					<ProductItem/>
+					<ProductItem product={product} key={product.id}/>
 				))}
 			</div>
 		</section>
