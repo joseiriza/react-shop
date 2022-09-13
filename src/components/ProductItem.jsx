9d5af2d6a6aa82bc906dc9c1dc295a0e533131ae
Jose Iriza <joseairiza@gmail.com>
2022-09-12 21:01:53 -0300

    using AppContext for addToCart state

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import '@styles/ProductItem.scss';
+import AppContext from '../context/AppContext';
 import addToCartButton from '@icons/bt_add_to_cart.svg';
 
 
 const ProductItem = ({ product }) => {
 
-	const [cart, setCart] = useState([]);
+	const { addToCart } = useContext(AppContext);
 
-	const handleClick = () => {
-		setCart([]);
+	const handleClick = item => {
+		addToCart(item);
 	}
 
 	return (
@@ -19,7 +20,7 @@ const ProductItem = ({ product }) => {
 					<p>${product.price}</p>
 					<p>{product.title}</p>
 				</div>
-				<figure onClick={handleClick}>
+				<figure onClick={() => handleClick(product)}>
 					<img src={addToCartButton} alt="" />
 				</figure>
 			</div>
