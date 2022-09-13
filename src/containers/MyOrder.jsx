616cf08c492ea68aef41e925ca769afff3274900
Jose Iriza <joseairiza@gmail.com>
2022-09-13 20:45:15 -0300

    using state to reflect myOrder status

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import OrderItem from '../components/OrderItem';
-import '../styles/MyOrder.scss';
-import flechita from '@icons/flechita.svg'
+import React, { useContext } from 'react';
+import OrderItem from '@components/OrderItem';
+import AppContext from '../context/AppContext';
+import '@styles/MyOrder.scss';
+import arrow from '@icons/flechita.svg';
 
 const MyOrder = () => {
+	const { state } = useContext(AppContext);
 	return (
 		<aside className="MyOrder">
 			<div className="title-container">
-				<img src={flechita} alt="arrow" />
+				<img src={arrow} alt="arrow" />
 				<p className="title">My order</p>
 			</div>
 			<div className="my-order-content">
-				<OrderItem />
+				{state.cart.map(product => (
+					<OrderItem product={product} key={`orderItem-${product.id}`} />
+				))}
 				<div className="order">
 					<p>
 						<span>Total</span>
