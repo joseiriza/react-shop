1ff038ce055337ded872bde44cd5196bdc943f5c
Jose Iriza <joseairiza@gmail.com>
2022-10-05 21:44:10 -0300

    added cart state in checkout page

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import OrderItem from '../components/OrderItem';
-import Menu from '../components/Menu';
+import React, { useContext } from 'react';
+import OrderItem from '../components/OrderItem'
+import AppContext from '../context/AppContext';
 import '../styles/Checkout.scss';
 
 const Checkout = () => {
+
+	const { state } = useContext(AppContext);
+
 	return (
 		<div className="Checkout">
 			<div className="Checkout-container">
 				<h1 className="title">My order</h1>
 				<div className="Checkout-content">
-					<div className="order">
-						<p>
-							<span>03.25.21</span>
-							<span>6 articles</span>
-						</p>
-						<p>$560.00</p>
-					</div>
+					{state.cart.map(product => (
+						<div className="order">
+							<OrderItem product={product} key={`orderItem-${product.id}`} />
+						</div>
+					))}
 				</div>
-				<OrderItem />
+				{/* <OrderItem /> */}
 			</div>
 		</div>
 	);
